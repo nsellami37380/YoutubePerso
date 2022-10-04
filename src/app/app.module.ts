be79fc1430e51a3ba696130e10e4841af84efd37
Noureddine <nourredinesellami@hotmail.com>
2022-10-04 13:36:14 +0200

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { VideoDetailComponent } from './video-detail/video-detail.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    VideoDetailComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
